0a9af34fb8a6875b19037db4ebbf6954221d7cce
Matthew Creek <matthew.creek27@gmail.com>
2022-11-17 19:08:13 -0600

    added variables for js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,34 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function(){
+
+const buttonTakeoff = document.getElementById('takeoff');
+const buttonLanding = document.getElementById('landing');
+const buttonAbort = document.getElementById('missionAbort');
+
+const statusFlight = document.getElementById('flightStatus');
+const heightShuttle = document.getElementById('spaceShuttleHeight');
+const backgroundShuttle = document.getElementById('shuttleBackground');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
