4820ba725a2b69636166150830ec9c3b11e3b802
Matthew Creek <matthew.creek27@gmail.com>
2022-11-17 20:10:16 -0600

    added height changes for up and down buttons, added formatting for left and right

--- scripts.js
+++ scripts.js
@@ -5,10 +5,15 @@ window.addEventListener('load', function(){
 const buttonTakeoff = document.getElementById('takeoff');
 const buttonLanding = document.getElementById('landing');
 const buttonAbort = document.getElementById('missionAbort');
+const buttonUp = document.getElementById('up');
+const buttonDown = document.getElementById('down');
+const buttonRight = document.getElementById('right');
+const buttonLeft = document.getElementById('left');
 
 const statusFlight = document.getElementById('flightStatus');
 const heightShuttle = document.getElementById('spaceShuttleHeight');
 const backgroundShuttle = document.getElementById('shuttleBackground');
+const shuttleImg = document.getElementById('rocket');
 
 buttonTakeoff.addEventListener('click', function(){
     let confirmLaunch = window.confirm('Confirm that the shuttle is ready for takeoff.');
@@ -33,22 +38,37 @@ buttonAbort.addEventListener('click', function(){
         backgroundShuttle.style.backgroundColor = 'green';
         heightShuttle.innerHTML = 0;
     }
-})
+});
 
+buttonUp.addEventListener('click', function(){
+    if (statusFlight.innerHTML === 'Shuttle in flight.'){
+        heightShuttle.innerHTML = add10K(Number(heightShuttle.innerHTML));
+        
+    }
+    shuttleImg.style.marginBottom = 0;
+});
 
+buttonDown.addEventListener('click', function(){
+    if (statusFlight.innerHTML === "Shuttle in flight." && Number(heightShuttle.innerHTML) > 0){
+        heightShuttle.innerHTML = remove10K(Number(heightShuttle.innerHTML));
+    }
+});
 
+buttonLeft.addEventListener('click', function(){
 
+});
 
+buttonRight.addEventListener('click', function(){
 
-
-
-
-
+});
 
 
 
 function add10K(num){
    return num + 10000
 }
+function remove10K(num){
+    return num - 10000
+ }
 
 })
